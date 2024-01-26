71dd8c76f051fb9fdef6951e78ee0736e606f9af
mstegema <mstegema@student.codam.nl>
2024-01-26 15:06:41 +0100

    split eating function to protect the times_eaten value with mutex

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: mstegema <mstegema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/04 15:42:34 by mstegema      #+#    #+#                 */
-/*   Updated: 2024/01/25 18:07:41 by mstegema      ########   odam.nl         */
+/*   Updated: 2024/01/26 14:01:15 by mstegema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,23 @@ static size_t	sleeping(t_philo *status)
 
 static size_t	eating(t_philo *status)
 {
+	print_message(status->data_copy, status->last_eaten, status->id, \
+	"is eating\n");
+	pthread_mutex_lock(&status->eaten_lock);
+	status->times_eaten++;
+	pthread_mutex_unlock(&status->eaten_lock);
+	while (get_time(status->data_copy) - status->last_eaten < \
+	status->data_copy->eat_time)
+	{
+		if (dying(status, EATING) == true)
+			return (KO);
+		usleep(5);
+	}
+	return (OK);
+}
+
+static size_t	grabbing(t_philo *status)
+{
 	if (pthread_mutex_lock(status->left_chopstick))
 	{
 		print_message(status->data_copy, get_time(status->data_copy), \
@@ -65,16 +82,8 @@ static size_t	eating(t_philo *status)
 			status->last_eaten = get_time(status->data_copy);
 			print_message(status->data_copy, get_time(status->data_copy), \
 			status->id, "grabbed a chopstick\n");
-			print_message(status->data_copy, status->last_eaten, status->id, \
-			"is eating\n");
-			status->times_eaten++;
-			while (get_time(status->data_copy) - status->last_eaten < \
-			status->data_copy->eat_time)
-			{
-				if (dying(status, EATING) == true)
-					return (KO);
-				usleep(5);
-			}
+			if (eating(status) == KO)
+				return (KO);
 			pthread_mutex_unlock(status->left_chopstick);
 			pthread_mutex_unlock(status->right_chopstick);
 		}
@@ -91,7 +100,7 @@ void	*routine(void *arg)
 		usleep(status->data_copy->eat_time / 2);
 	while (status->fatality == false)
 	{
-		if (eating(status) == KO)
+		if (grabbing(status) == KO)
 			return (NULL);
 		if (sleeping(status) == KO)
 			return (NULL);
